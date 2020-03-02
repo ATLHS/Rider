b321703d550a290b4de3122d4f5165dd17c700ae
Sofiane <s-attilah@hotmail.com>
2020-03-02 20:31:08 +0100

    Test automatic deploy on heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/signup", (req, res) => {
     res.json(signup);
 })
 
-if(process.env.NODE_ENV === "production"){
+if(process.env.NODE_ENV === "production"){ 
     app.use(express.static(path.join(__dirname, "./client/build")));
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname + "../client/build/index.html"));
